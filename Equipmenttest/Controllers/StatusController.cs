7a8d63751e54711bbe0fa340358b7766a711a888
Petra Knip <petra@capteur.nl>
2019-08-07 17:03:19 +0200

    first attempt equipmenttables in cssgrid

---
+++ Equipmenttest/Controllers/StatusController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Equipmenttest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Equipmenttest.Controllers
+{
+    public class StatusController : Controller
+    {
+        private readonly TestDataProvider provider;
+
+        public StatusController(TestDataProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        public IActionResult Index()
+        {
+            var data = provider.CreateRooms(4);
+
+            return View(data);
+        }
+
+        public IActionResult Alternative()
+        {
+            return View();
+        }
+    }
+}
---
+++ Equipmenttest/Models/TestDataProvider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Equipmenttest.Models
+{
+    public class TestDataProvider
+    {
+        public Random rng = new Random(34982174);
+
+        public Dictionary<string, Dictionary<string, Dictionary<int, string>>> CreateRooms(int count)
+        {
+            var result = new Dictionary<string, Dictionary<string, Dictionary<int, string>>>();
+            for (var i = 0; i < count; i++)
+            {
+                result[$"Room {i}"] = CreateDeviceTypes(rng.Next(1, 10));
+            }
+            return result;
+        }
+
+        public Dictionary<string, Dictionary<int, string>> CreateDeviceTypes(int count)
+        {
+            var result = new Dictionary<string, Dictionary<int, string>>();
+            for (var i = 0; i < count; i++)
+            {
+                result[$"DeviceType {i}"] = CreateDevices(rng.Next(3, 100));
+            }
+            return result;
+        }
+
+        public Dictionary<int, string> CreateDevices(int count)
+        {
+            var result = new Dictionary<int, string>();
+            for (var i = 0; i < count; i++)
+            {
+                result[i] = $"Device-{i}";
+            }
+            return result;
+        }
+    }
+}
